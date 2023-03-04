e22bab0ca1dc79a83cb6e8400debd0887a46db4e
theydvgaurav <gauravyadav2609@gmail.com>
2023-03-05 00:42:51 +0530

    Upadated POST API

--- Controllers/contacts.js
+++ Controllers/contacts.js
@@ -18,7 +18,7 @@ addContact = async (req, res) => {
         author: req.user.id
     })
     newContact.save().then((data) => {
-        return res.send(data)
+        return res.send([data])
     })
         .catch(error => {
             return res.status(500).json(error)
