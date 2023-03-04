28f2413f8576153d40e2f9aac3a62c712378c7e5
theydvgaurav <gauravyadav2609@gmail.com>
2023-03-04 23:09:44 +0530

    renamed routes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const log = console.log;
 const app = express();
 const cors = require('cors');
 const PORT = process.env.PORT || 5000;
-const routes = require('./routes/index')
+const routes = require('./Routes/index')
 
 app.use(express.json());
 app.use(cors());
